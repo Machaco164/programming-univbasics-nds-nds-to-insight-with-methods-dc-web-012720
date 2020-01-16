1cec6ee87551aefb0fdeb49d1e979f7affa33bc6
Raul A Rozo <raul.a.rozo.o@gmail.com>
2020-01-16 20:28:27 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -24,5 +24,18 @@ end
 # { directorOne => allTheMoneyTheyMade, ... }
 def directors_totals(nds)
   result = {}
-   puts nds
+  row_index = 0
+  while row_index < nds.count do
+    grand_total = 0
+    element_index = 0 
+    name = nds[row_index][:name]
+    name
+        while element_index < nds[row_index][:movies].count do
+        grand_total += nds[row_index][:movies][element_index][:worldwide_gross]
+        element_index += 1 
+        end
+  result[name] = grand_total
+  row_index += 1
+end
+ result
 end
